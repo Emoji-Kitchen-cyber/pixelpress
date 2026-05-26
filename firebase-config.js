8304cae7f13fc1785a491dcabdc3b679a438b96b
Emoji-Kitchen-cyber <jahangirgujjar056@gmail.com>
2026-05-26 15:44:28 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDne_dR6VLBQGQx22ULu5Zo05f0yepBUsM",
-  authDomain: "pixelpress-abd6c.firebaseapp.com",
-  projectId: "pixelpress-abd6c",
-  storageBucket: "pixelpress-abd6c.firebasestorage.app",
-  messagingSenderId: "1023588720890",
-  appId: "1:1023588720890:web:d36a32ad513d267894a943"
-};

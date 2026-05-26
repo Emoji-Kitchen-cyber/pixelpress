f75095d69cdc8ddf1d1ae72f5fa6d87c8a53d1dd
Emoji-Kitchen-cyber <jahangirgujjar056@gmail.com>
2026-05-26 22:00:14 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'pixelpress-v4';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/index.html','/css/style.css','/js/common.js'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});

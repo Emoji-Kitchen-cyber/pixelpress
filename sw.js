b3f97c8d017bc79bc18e9fb9aff0a2adf6d8ba53
Emoji-Kitchen-cyber <jahangirgujjar056@gmail.com>
2026-05-26 22:02:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+const CACHE = 'pixelpress-v4';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/index.html','/css/style.css','/js/common.js'])));
 });
-
-self.addEventListener('activate', () => {
-  self.clients.claim();
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
-
-self.addEventListener('fetch', () => {});
